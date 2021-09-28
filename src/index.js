d1721f3431266854371b51d479fa7d40c2d67c61
AndreyMandybura <80692584+AndreyMandybura@users.noreply.github.com>
2021-09-28 18:35:05 +0300

    ДЗ 12

--- src/index.js
+++ src/index.js
@@ -1,1 +1,43 @@
-import './sass/main.scss';
+import './css/styles.css';
+import refs from './js/refs';
+import API from './js/fetchCountries';
+
+import allCountriesMarkup from './tamplates/allCountriesMarkup.hbs';
+import countryCardMarkup from './tamplates/countryCardMarkup.hbs';
+
+import { debounce } from 'lodash';
+import { Notify, Loading } from 'notiflix';
+
+const { success, warning, failure, info } = Notify;
+
+refs.searchInput.addEventListener('input', debounce(onCountriesSearch, 500));
+
+
+function onCountriesSearch(e) {
+  API.fetchCountries(e.target.value).then(inputValidation)
+}
+
+function inputValidation(country) {
+  if (country.status === 404) {
+    refs.country.innerHTML = '';
+    serverResponse(failure, 'There is no such country!');
+    return
+  } else if (country.length > 10) {
+    refs.country.innerHTML = '';
+    serverResponse(warning, 'Too many matches found. Please enter a more specific qery!');
+    return
+  } else if (country.length > 1) {
+    refs.country.innerHTML = allCountriesMarkup(country);
+    serverResponse(info, 'Enter full name');
+    return
+    } else if (country.length === 1) {
+    refs.country.innerHTML = countryCardMarkup(country);
+    serverResponse(success, 'You found your country!');
+    refs.searchInput.value = '';
+  }
+}
+
+function serverResponse(r, message) {
+  r(message);
+  Loading.remove();
+}
---
+++ src/js/refs.js
@@ -0,0 +1,5 @@
+export default {
+  searchInput: document.querySelector('.js-input'),
+  countriesList: document.querySelector('.js-countries-list'),
+  country: document.querySelector('.js-country'),
+}
